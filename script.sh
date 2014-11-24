c9ec5779d159b1107cc79d727fbca4bbf6fd1401
Robert O'Connell <oconnellrobertc@gmail.com>
2014-11-24 16:01:26 +0000

    Initial script

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -d /tmp/activeTables ]; then
+	mkdir /tmp/activeTables
+fi
+cd /tmp/activeTables
+wget -Nq https://www.dshield.org/block.txt -O /tmp/activeTables/block.txt
+cat block.txt | grep -P '^[^#S]' | awk '{print $1 "/" $3}' # | xargs -0 -I ip -n 1 -d "\n" ipset add bad_nets ip
+
