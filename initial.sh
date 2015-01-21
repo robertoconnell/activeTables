06c11523e61f954f9029618f28f5c8164d91a713
f <f@f>
2015-01-21 16:43:11 +0000

    command

--- initial.sh
+++ initial.sh
@@ -3,7 +3,14 @@ if [[ $EUID -ne 0 ]]; then
     echo "This script requires root access to function."
     exit 1
 fi
-(which ipset >/dev/null && echo "IPset is installed.") || echo "Install IPset, it should be in your distrobution's repositories."; exit 1
+which ipset >/dev/null
+
+if [ $? -eq 0 ]; then
+    echo "IPset is installed."
+else
+    echo "Install IPset, it should be in your distrobution's repositories."; exit 1
+fi
+
 if [ ! -d /tmp/activeTables ]; then
     mkdir /tmp/activeTables
 fi
@@ -13,8 +20,4 @@ else
 	echo "Creating ipset table 'activeTable'" 
 	ipset create activeTable nethash
 fi
-#cd /tmp/activeTables
-#echo "Downloading blocklist from dshield.org"
-#wget -Nq https://www.dshield.org/block.txt -O /tmp/activeTables/block.txt
-#cat block.txt | grep -P '^[^#S]' | awk '{print $1 "/" $3}' # | xargs -0 -I ip -n 1 -d "\n" ipset add bad_nets ip
-
+echo "iptables -I OUTPUT -m set --match-set activeTable dst -j REJECT "
